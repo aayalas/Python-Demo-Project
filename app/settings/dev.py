8a496657dc50b25a1ce38e1ed8a8a4b7430cf611
AlexAyalaS <alex.ayalas@gmail.com>
2023-09-09 21:30:40 -0600

    14th commit

--- app/settings/dev.py
+++ app/settings/dev.py
@@ -7,9 +7,7 @@ DEBUG = 'RENDER' not in os.environ
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = []
-
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')if RENDER_EXTERNAL_HOSTNAME: ALLOWED_HOST.append(RENDER_EXTERNAL_HOSTNAME)
+ALLOWED_HOSTS = ['*']
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
