acda583ec53d7ae24607a60f145e2d70919e5ae4
AlexAyalaS <alex.ayalas@gmail.com>
2023-09-09 20:52:03 -0600

    10th commit

--- app/settings/dev.py
+++ app/settings/dev.py
@@ -1,13 +1,15 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-x3!jyxxig^-qz2e0dz7u67mj)n$!ri5n7c5)as1*iqqgajw4hp"
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')if RENDER_EXTERNAL_HOSTNAME: ALLOWED_HOST.append(RENDER_EXTERNAL_HOSTNAME)
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
